8f1c4bd872e820912b3b42b5668b1f42a8c628ee
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-25 05:40:11 +0900

    remove typings dir and use tsconfig importation

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/es6-promise/index.d.ts" />
-/// <reference path="browser/ambient/webaudioapi/waa/index.d.ts" />
-/// <reference path="browser/ambient/webrtc/mediastream/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/es6-promise/index.d.ts" />
-/// <reference path="main/ambient/webaudioapi/waa/index.d.ts" />
-/// <reference path="main/ambient/webrtc/mediastream/index.d.ts" />
