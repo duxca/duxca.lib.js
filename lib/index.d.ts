ab81f0a43f9eb2a51373dfb35eced64000e23ca7
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-24 22:54:29 +0900

    update

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,6 @@
 import * as _Statictics from "duxca.lib.statictics.js";
+import * as _Signal from "duxca.lib.signal.js";
 export declare namespace lib {
     var Statictics: typeof _Statictics;
+    var Signal: typeof _Signal;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-
 import * as _Statictics from "duxca.lib.statictics.js";
+import * as _Signal from "duxca.lib.signal.js";
+
 export namespace lib {
-  export var Statictics = _Statictics
+  export var Statictics = _Statictics;
+  export var Signal = _Signal;
 }
