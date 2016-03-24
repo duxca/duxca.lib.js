1843652dc96b3ccc949b21fd54db5f0bbe5deeed
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-25 05:21:31 +0900

    add typings dir

---
+++ typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/webaudioapi/waa/index.d.ts" />
+/// <reference path="browser/ambient/webrtc/mediastream/index.d.ts" />
+/// <reference path="browser/definitions/bluebird/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main/ambient/webaudioapi/waa/index.d.ts" />
+/// <reference path="main/ambient/webrtc/mediastream/index.d.ts" />
+/// <reference path="main/definitions/bluebird/index.d.ts" />
