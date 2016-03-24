cf5f3423051a3dc936075236609261363086a151
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-24 23:42:18 +0900

    add Wave

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,8 +1,10 @@
 import * as _Statictics from "duxca.lib.statictics.js";
 import * as _Signal from "duxca.lib.signal.js";
 import * as _RecordBuffer from "duxca.lib.recordbuffer.js";
+import * as _Wave from "duxca.lib.wave.js";
 export declare namespace lib {
     var Statictics: typeof _Statictics;
     var Signal: typeof _Signal;
     var RecordBuffer: typeof _RecordBuffer;
+    var Wave: typeof _Wave;
 }
