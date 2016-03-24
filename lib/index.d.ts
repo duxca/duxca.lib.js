04c587f1391589b90343f3bda7551e90b786f5ed
Legokcihi Duckscallion <legokichi@gmail.com>
2016-03-25 05:53:01 +0900

    trpleslash to tsconfig & add es6-promise

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,3 @@
-/// <reference path="../typings/browser.d.ts" />
 declare class Hoge {
     constructor();
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-/// <reference path="../typings/browser.d.ts"/>
-
 class Hoge {
   constructor(){
     Promise.resolve(null);
