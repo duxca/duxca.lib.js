441ab1599ec2a46c19635ceb382710e5675d1804
Legokcihi Duckscallion <legokichi@gmail.com>
2016-12-21 13:55:59 +0900

    add storaged input handler

--- lib/XStream2JQuery.d.ts
+++ lib/XStream2JQuery.d.ts
@@ -5,3 +5,22 @@ export declare function createResizeRatioStream($elm: JQuery): Stream<number>;
 export declare function touchstart($elm: JQuery, selector?: string): Stream<JQueryEventObject>;
 export declare function touchmove($elm: JQuery, selector?: string): Stream<JQueryEventObject>;
 export declare function touchend($elm: JQuery, selector?: string): Stream<JQueryEventObject>;
+export declare type JSONString = string;
+export interface JSONStorage {
+    getItem(key: string): JSONString | null;
+    setItem(key: string, data: JSONString): void;
+}
+export declare function getInputStreamWithStorage<T>(Storage: JSONStorage, $elm: JQuery, key: string, event?: string): Stream<T>;
+export declare function getCombinedInputStreamWithStorage<T>(Storage: JSONStorage, o: {
+    [P in keyof T]: JQuery;
+}): Stream<T>;
+export declare function getCombinedSelectStreamWithStorage<T extends {
+    [key: string]: {
+        $: JQuery;
+        opt: {
+            [id: string]: string;
+        };
+    };
+}>(Storage: JSONStorage, o: T): Stream<{
+    [P in keyof T]: keyof T[P]["opt"];
+}>;
